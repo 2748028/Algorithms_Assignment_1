e3d5929911f24e2432f33e10f29a0feb9b7d1ade
2748028 <118025691+2748028@users.noreply.github.com>
2023-03-28 11:57:10 -0400

    Init commit.

---
+++ Algo Assignment 1/Algo Assignment 1.cpp
@@ -0,0 +1,38 @@
+// Algo Assignment 1.cpp : Defines the entry point for the console application.
+#include "stdafx.h"
+#include "iostream"
+#include "string"
+using namespace std;
+
+int complexFunction (double n)
+{
+	cout << "Please enter a multiplication value for 'n' times: \n" <<  Sum;
+	cin >> n;
+	
+	cout << n;
+	int negativeSumSquare = 0;
+	for (double i = 1; i <= n; i++)
+		Sum = Sum - i*i;
+		cout << Sum;
+
+	return 0;
+}
+
+void printInfo()
+{
+	cout << "This is a practice program.";
+}
+
+//-------------------------------//
+
+int main()
+{
+	int pumps;
+	//printInfo()
+
+	complexFunction (1774785777700);
+
+	cin >> pumps;
+
+	return 0;
+}
